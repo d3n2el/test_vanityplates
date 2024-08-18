a3bec642f5f7fdb907fcf4a1cff5b0484149bed8
d3n2el <118548099+d3n2el@users.noreply.github.com>
2024-08-18 22:20:06 +0200

    Add files via upload

--- test_plates.py
+++ test_plates.py
@@ -6,14 +6,14 @@ def test_num():
     assert is_valid("dan12er") == False
 
 def test_length():
-    assert is_valid("CS50") == True
-    assert is_valid("01dan") == False
-    assert is_valid("CS05") == False
-    assert is_valid("dan12er") == False
+    assert is_valid("ab") == True
+    assert is_valid("a") == False
+    assert is_valid("abcdfeffgg") == False
+    assert is_valid("qwerty") == True
 
 def test_firstletters():
-    assert is_valid("dan4") == True
-    assert is_valid("01dan") == False
+    assert is_valid("da4") == True
+    assert is_valid("ef1") == True
     assert is_valid("05ahs") == False
     assert is_valid("34fjfje") == False
 
